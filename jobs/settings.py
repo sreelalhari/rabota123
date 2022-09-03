becd7c265f26b6a33911ef1fed4a167fbcca42b1
Sreelal <sreelalsree2001@gmail.com>
2022-09-03 21:47:51 +0530

    commit message

--- jobs/settings.py
+++ jobs/settings.py
@@ -134,6 +134,6 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
